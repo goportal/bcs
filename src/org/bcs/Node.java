f6b0073deee4279e9ee301314ed9b49648d77517
portal <gabriel.o.portal@gmail.com>
2019-05-15 10:01:29 -0300

    Utils RSA implementation

--- src/org/bcs/Node.java
+++ src/org/bcs/Node.java
@@ -1,5 +1,8 @@
 package org.bcs;
 
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Scanner;
@@ -34,6 +37,9 @@ public class Node {
 //            System.out.println(" 6 - Start a server");
             System.out.println(" 5 - Connect to peer");
             System.out.println(" 6 - List connected nodes");
+            
+            System.out.println("7 - KeyPair test");
+            
 
             int option = Integer.parseInt(sysIn.nextLine());
 
@@ -110,15 +116,27 @@ public class Node {
 
                     break;
 
-//                case 8:
-//
-//                    List<String> connectedNodes = cServer.getConnectedNodes();
-//
-//                    for (int i = 0; i < connectedNodes.size(); i++) {
-//                        System.err.println("Node: " + i + "  Ip: " + connectedNodes.get(i));
-//                    }
-//
-//                    break;
+                case 7:
+
+                    KeyPair keys = this.utils.generateKeyPair();
+                    
+                    PublicKey pubKey = keys.getPublic();
+                    PrivateKey privKey = keys.getPrivate();
+                    
+                    String content = "This is a text";
+                    
+                    System.out.println("plain text is: "+ content);
+                    
+                    byte[] cyText = utils.criptografa(content, pubKey);
+                    
+                    System.out.println("The encripted text is: "+cyText.toString());
+                    
+                    String decripted = utils.decriptografa(cyText, privKey);
+                    
+                    System.out.println("The decripted text is: "+decripted);
+                    
+                    
+                    break;
 
                 default:
                     System.out.println("Wrong choice! try again.");
--- src/org/bcs/Utils.java
+++ src/org/bcs/Utils.java
@@ -1,8 +1,16 @@
 package org.bcs;
 
 import java.io.UnsupportedEncodingException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
 
 /**
  *
@@ -31,5 +39,64 @@ public class Utils {
         return textCypher;
 
     }
+    
+    
+    public KeyPair generateKeyPair(){
+        
+        KeyPair pair = null;
+        
+        try {
+            
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(1024);
+            pair = keyGen.generateKeyPair();
+            
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+           
+        
+//            PrivateKey priv = pair.getPrivate();
+//            PublicKey pub = pair.getPublic();
+            
+        return pair;
+        
+    }
+    
+    public byte[] criptografa(String texto, PublicKey chave) {
+        byte[] cipherText = null;
+     
+        try {
+            final Cipher cipher = Cipher.getInstance("RSA");
+            // Criptografa o texto puro usando a chave Púlica
+            cipher.init(Cipher.ENCRYPT_MODE, chave);
+            cipherText = cipher.doFinal(texto.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+     
+        return cipherText;
+    }
+  
+    /**
+     * Decriptografa o texto puro usando chave privada.
+     */
+    public String decriptografa(byte[] texto, PrivateKey chave) {
+        byte[] dectyptedText = null;
+     
+        try {
+            final Cipher cipher = Cipher.getInstance("RSA");
+            // Decriptografa o texto puro usando a chave Privada
+            cipher.init(Cipher.DECRYPT_MODE, chave);
+            dectyptedText = cipher.doFinal(texto);
+  
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+  
+        return new String(dectyptedText);
+    }
+    
+    
     
 }
